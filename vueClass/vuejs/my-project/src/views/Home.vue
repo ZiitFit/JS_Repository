c05f3c839d5f1ed46e3f6c751c5501d6ff85b3b9
betty <betty@marketdesigners.com>
2020-01-22 00:26:24 +0900

    feat: vue todolist

--- vueClass/vuejs/my-project/src/views/Home.vue
+++ vueClass/vuejs/my-project/src/views/Home.vue
@@ -1,18 +1,30 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TodoHeader/>
+    <TodoInput/>
+    <TodoList/>
+    <TodoFooter/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import TodoHeader from '@/components/TodoHeader.vue'
+import TodoInput from '@/components/TodoInput.vue'
+import TodoList from '@/components/TodoList.vue'
+import TodoFooter from '@/components/TodoFooter.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    HelloWorld,
+    TodoHeader,
+    TodoInput,
+    TodoList,
+    TodoFooter
   }
 }
 </script>
