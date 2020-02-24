6e85a33c409bba96eeaf6be88e9d660deafaf814
betty <betty@marketdesigners.com>
2020-02-25 00:15:31 +0900

    footer 컴포넌트

--- vueClass/vuejs/my-project/src/components/TodoFooter.vue
+++ vueClass/vuejs/my-project/src/components/TodoFooter.vue
@@ -1,15 +1,28 @@
 <template>
-  <div>
-      footer
+  <div class="btnfooter">
+      <span v-on:click="clearTodo">Clear All</span>
   </div>
 </template>
 
 <script>
 export default {
-
+  methods: {
+    clearTodo: function() {
+      localStorage.clear();
+    }
+  }
 }
 </script>
 
-<style>
-
+<style scoped>
+  .btnfooter{
+    width:80%;
+    height:50px;
+    margin: 0 auto;
+    line-height:50px;
+    background:#eaeaea;
+  }
+  .btnfooter span {
+    color:red;
+  }
 </style>
