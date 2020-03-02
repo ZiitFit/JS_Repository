bccae0d3051d7d0db04bc05cf9db2692ac0fba3f
betty <betty@marketdesigners.com>
2020-03-03 00:02:15 +0900

    toolbar 라우터링크 / 서브페이지 라우터구현

--- vueClass/vuejs/vue-news/src/App.vue
+++ vueClass/vuejs/vue-news/src/App.vue
@@ -1,12 +1,17 @@
 <template>
   <div id="app">
-   <router-view></router-view>
+    <tool-bar></tool-bar>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
+import ToolBar from './components/ToolBar'
 
 export default {
+  components: {
+    ToolBar,
+  }
   // methods: {
   //   fetchData() {
   //     console.log('hello');
@@ -16,12 +21,8 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  margin:0;
+  padding:0;
 }
 </style>
---
+++ vueClass/vuejs/vue-news/src/components/ToolBar.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="header">
+      <router-link to="/news">News</router-link> |
+      <router-link to="/ask">Ask</router-link> |
+      <router-link to="/jobs">Jobs</router-link> |
+      <router-link to="/item">Item</router-link> |
+      <router-link to="/user">User</router-link>
+  </div>
+</template>
+
+<style scoped>
+.header {
+    display:block;
+    padding: 8px;
+    color:#fff;
+    background:#42b883;
+}
+.header .router-link-exact-active {
+    color: #35495e;
+}
+.header a{
+    color:#fff;
+}
+</style>
