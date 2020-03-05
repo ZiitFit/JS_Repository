fb231a296fa6653532c1bc6f63941660d278379d
betty <betty@marketdesigners.com>
2020-03-06 00:29:40 +0900

    store적용 - vuex 모듈화 / state, actions, mutations 적용

---
+++ vueClass/vuejs/vue-news/src/store/index.js
@@ -0,0 +1,56 @@
+import Vue from 'vue'
+import Vuex from 'vuex';
+import { fetchNewsList, fetchAskList, fetchJobsList } from '../api/index.js';
+
+
+Vue.use(Vuex);
+
+export const store = new Vuex.Store({
+  state: {
+		news: [],
+		ask: [],
+		jobs: [],
+	},
+	mutations:{
+		SET_NEWS(state, news) {
+			state.news = news;
+		},
+		SET_ASK(state, ask) {
+			state.ask = ask;
+		},
+		SET_JOBS(state, jobs) {
+			state.jobs = jobs;
+		},
+	},
+	actions: {
+		FETCH_NEWS(context) {
+			fetchNewsList()
+				.then(response => {
+					console.log(response)
+					context.commit('SET_NEWS', response.data);
+				})
+				.catch(error => {
+					console.log(error)
+				})
+		},
+		FETCH_ASK(context) {
+			fetchAskList()
+				.then(response => {
+					console.log(response)
+					context.commit('SET_ASK', response.data)
+				})
+				.catch(error => {
+					console.log(error)
+				})
+		},
+		FETCH_JOBS({ commit }) {
+			fetchJobsList()
+				.then(({ data }) => {
+					commit('SET_JOBS', data);
+				})
+				.catch(error => {
+					console.log(error)
+				})
+		},
+	}
+})
