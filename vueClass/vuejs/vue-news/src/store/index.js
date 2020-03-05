7b25006b8a9ad4accb610354fd29d9b1217b108f
betty <betty@marketdesigners.com>
2020-03-06 00:43:38 +0900

    actions, mutations 속성 모듈화

--- vueClass/vuejs/vue-news/src/store/index.js
+++ vueClass/vuejs/vue-news/src/store/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Vuex from 'vuex';
-import { fetchNewsList, fetchAskList, fetchJobsList } from '../api/index.js';
+// import { fetchNewsList, fetchAskList, fetchJobsList } from '../api/index.js';
+import mutations from './mutations.js';
+import actions from './actions.js';
 
 
 Vue.use(Vuex);
@@ -17,46 +19,6 @@ export const store = new Vuex.Store({
 			return state.ask;
 		}
 	},
-	mutations:{
-		SET_NEWS(state, news) {
-			state.news = news;
-		},
-		SET_ASK(state, ask) {
-			state.ask = ask;
-		},
-		SET_JOBS(state, jobs) {
-			state.jobs = jobs;
-		},
-	},
-	actions: {
-		FETCH_NEWS(context) {
-			fetchNewsList()
-				.then(response => {
-					console.log(response)
-					context.commit('SET_NEWS', response.data);
-				})
-				.catch(error => {
-					console.log(error)
-				})
-		},
-		FETCH_ASK(context) {
-			fetchAskList()
-				.then(response => {
-					console.log(response)
-					context.commit('SET_ASK', response.data)
-				})
-				.catch(error => {
-					console.log(error)
-				})
-		},
-		FETCH_JOBS({ commit }) {
-			fetchJobsList()
-				.then(({ data }) => {
-					commit('SET_JOBS', data);
-				})
-				.catch(error => {
-					console.log(error)
-				})
-		},
-	}
+	mutations,
+	actions,
 })
