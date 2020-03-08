d749c49048c30471fd29fc8fb5a5386465cfe346
betty <betty@marketdesigners.com>
2020-03-08 19:49:42 +0900

    다른폴더에 혼자 다시 작업해보기( store 구현 전까지)
    프로젝트 셋업, 라우터 구현, api구현

---
+++ vueClass/vuejs/news-test/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <Tool-bar></Tool-bar>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import ToolBar from './components/ToolBar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ToolBar
+  }
+}
+</script>
+
+<style>
+body {
+  margin:0;
+  padding:0;
+}
+</style>
---
+++ vueClass/vuejs/news-test/src/views/JobsView.vue
@@ -0,0 +1,31 @@
+<template>
+	<div>
+		<p v-for="job in jobs" v-bind:key="job.id"> {{ job.title }} </p>
+	</div>
+</template>
+
+<script>
+import { fetchJobsList } from '../api/index.js'
+
+export default {
+	data() {
+		return {
+			jobs: []
+		}
+	},
+	created() {
+		var vm = this;
+		fetchJobsList()
+			.then(function(response) {
+				vm.jobs = response.data
+			})
+			.catch(function(error) {
+				console.log(error)
+			})
+	}
+}
+</script>
+
+<style>
+
+</style>
