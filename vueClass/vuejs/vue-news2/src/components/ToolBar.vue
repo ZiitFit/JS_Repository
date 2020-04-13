af56a286e1612e7909e6112273367d96f963bea7
betty <betty@marketdesigners.com>
2020-04-14 00:05:58 +0900

    hacker-news axios api 함수구조화

---
+++ vueClass/vuejs/vue-news2/src/components/ToolBar.vue
@@ -0,0 +1,26 @@
+<template>
+	<div class="header">
+		<router-link to="/news">News</router-link> |
+		<router-link to="/ask">Ask</router-link> |
+		<router-link to="/jobs">Jobs</router-link> |
+		<router-link to="/item">Item</router-link> |
+		<router-link to="/user">User</router-link>
+	</div>
+</template>
+
+
+<style scoped>
+.header{
+	display:flex;
+	padding:8px;
+	background-color:#42b883;
+	color:#fff;
+}
+.header .router-link-exact-active{
+	color:#35495e;
+}
+.header a{
+	margin: 0 5px;
+	color:#fff;
+}
+</style>
---
+++ vueClass/vuejs/vue-news2/src/views/AskView.vue
@@ -0,0 +1,32 @@
+<template>
+	<div>
+		<p v-for="ask in asks" :key="ask.id">{{ ask }}</p>
+	</div>
+</template>
+
+<script>
+import { fetchAskList } from '../api/index.js';
+
+export default {
+	data() {
+		return{
+			asks :[]
+		}
+	},
+	created() {
+		var vm = this;
+		fetchAskList()
+			.then(function(response){
+				console.log(response)
+				vm.asks = response.data
+			})
+			.catch(function(error){
+				console.log(error)
+			})
+	}
+}
+</script>
+
+<style>
+
+</style>
