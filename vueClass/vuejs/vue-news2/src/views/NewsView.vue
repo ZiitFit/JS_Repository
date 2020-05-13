37e25085ba773c401447a8739319b30311bc1322
betty <betty@marketdesigners.com>
2020-05-13 22:54:24 +0900

    공통 컴포넌트 제작

--- vueClass/vuejs/vue-news2/src/views/NewsView.vue
+++ vueClass/vuejs/vue-news2/src/views/NewsView.vue
@@ -1,69 +1,15 @@
 <template>
 	<div>
-		<ul class="news-list">
-			<li v-for="(user, idx) in this.$store.state.news" :key="idx" class="post">
-				<!-- 포인트 영역 -->
-				<div class="points">
-					{{ user.points}}
-				</div>
-				<!-- 기타 정보영역 -->
-				<div>
-					<p class="news-title">
-						<a v-bind:href="user.url">
-							{{ user.title }}
-						</a>
-					</p>
-					<small class="link-text">
-						by
-						<!-- <router-link v-bind:to="'/user' + user.user">{{ user.user }}</router-link> -->
-						<router-link v-bind:to="`/user/${user.user}`" class="link-text">{{ user.user }}</router-link>
-					</small>
-				</div>
-			</li>
-		</ul>
+		<list-item></list-item>
 	</div>
 </template>
 
 <script>
+import ListItem from '../components/ListItem.vue'
 
 export default {
-	// data() {
-	// 	return {
-	// 		users: []
-	// 	}
-	// },
-	created() {
-		this.$store.dispatch('FETCH_NEWS');
-		// fetchNewsList()
-		// 	.then(response => this.users = response.data)
-		// 	.catch(error => console.log(error))
+	components: {
+		ListItem,
 	}
 }
 </script>
-
-<style scoped>
-.news-list{
-	margin:0;
-	padding:0;
-}
-.post{
-	display:flex;
-	border-bottom:1px solid #bbb;
-	list-style:none;
-	align-items:center
-}
-.points{
-	display:flex;
-	width:80px;
-	height:60px;
-	align-items:center;
-	justify-content:center;
-	color:#42b883;
-}
-.news-title{
-	margin:0;
-}
-.link-text{
-	color:#828282;
-}
-</style>
