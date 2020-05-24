28b94e1c1c7f20784cad7940b2af7567419405c8
betty <betty@marketdesigners.com>
2020-05-24 20:35:18 +0900

    사용자 프로필 컴포넌트 데이터 흐름처리1

---
+++ vueClass/vuejs/vue-news2/src/components/UserProfile.vue
@@ -0,0 +1,45 @@
+<template>
+	<div class="user-container">
+		<div>
+			<i class="fas fa-user"></i>
+		</div>
+		<div class="user-description">
+			<div>{{ userInfo.id }}</div>
+			<!-- <router-link :to="`/user/${userInfo.user}`">
+				{{ userInfo.id }}
+			</router-link> -->
+			<div class="time">
+				{{ userInfo.created }}
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	computed: {
+		userInfo() {
+			return this.$store.state.user;
+		}
+	},
+}
+</script>
+
+<style scoped>
+.user-container{
+	display:flex;
+	padding:16px;
+	align-items:center;
+}
+.fa-user{
+	font-size:40px;
+}
+.user-description{
+	padding-left:10px;
+}
+.time{
+	font-size:13px;
+}
+</style>>
+
+</style>
